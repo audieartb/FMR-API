30dc2840e38a843fac93183c6442fd79341b4aff
audieartb <aj.artavia19@gmail.com>
2021-02-27 23:35:15 -0600

    rm

--- routes/__init__.py
+++ routes/__init__.py
@@ -6,4 +6,4 @@ routes = Blueprint('routes', __name__)
 from .Movies import movies_api
 from .Restaurants import restaurants_api
 from .User import users_api
-mongoUri = 'mongodb+srv://audie-artavia:newmongopass@clustersc-609.ucywa.gcp.mongodb.net/Jumanji?retryWrites=true&w=majority'
+
